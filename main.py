41302c07d889b43104bdbce31ce28e1282f3b8f2
Denis Yakovlev <denn.yakovlev@gmail.com>
2020-03-08 16:33:07 +0300

    Simplified

--- main.py
+++ main.py
@@ -1,34 +1,14 @@
-from lxml.html import HtmlElement
 from selenium import webdriver
 from lxml import html
-from datetime import date, datetime
-from typing import List
+from bs4 import BeautifulSoup
 import json
 
 
-class Article:
-    url: str
-    title: str
-    category: str
-    pub_date: str
-    authors: List[str]
-    keywords: List[str]
-
-    def __init__(self, url, title, category, pub_date, authors, keywords):
-        self.url = url
-        self.title = title
-        self.category = category
-        self.pub_date = pub_date
-        self.authors = authors
-        self.keywords = keywords
-
-
 def get_page_html_element(driver):
-    html_element: HtmlElement = html.fromstring(driver.page_source)
-    return html_element
+    return html.fromstring(driver.page_source)
 
 
-def get_articles(driver, base_url):
+def get_articles_meta(driver, base_url):
     articles = []
     for page in range(1, 7):
         driver.get(f"{base_url}/articles?searchType=journalSearch&sort=PubDate&page={page}")
@@ -40,34 +20,21 @@ def get_articles(driver, base_url):
             driver.get(base_url + link)
             html_element = get_page_html_element(driver)
             articles.append(
-                Article(
-                    url=base_url + link,
-                    title=str(html_element.xpath("//h1[@data-test='article-title']/text()")[0]),
-                    category=str(html_element.xpath("//li[@data-test='article-category']/text()")[0]),
-                    pub_date=str(html_element.xpath("//time[@itemprop='datePublished']/@datetime")[0]),
-                    authors=html_element.xpath("//ul[@data-test='authors-list']/li/span[1]/a/text()"),
-                    keywords=html_element.xpath("//ul[@class='c-article-subject-list']/li/span/text()")
-                )
+                {
+                    "url": base_url + link,
+                    "title": str(html_element.xpath("//h1[@data-test='article-title']/text()")[0]),
+                    "category": str(html_element.xpath("//li[@data-test='article-category']/text()")[0]),
+                    "pub_date": str(html_element.xpath("//time[@itemprop='datePublished']/@datetime")[0]),
+                    "authors": html_element.xpath("//ul[@data-test='authors-list']/li/span[1]/a/text()"),
+                    "keywords": html_element.xpath("//ul[@class='c-article-subject-list']/li/span/text()")
+                }
             )
     return articles
-
-
-def save_json(file_name, content):
-    with open(file_name, 'w', encoding='utf-8') as f:
-        json.dump(content, f, indent=2)
 
 
 if __name__ == '__main__':
     base_url = "https://journalofbigdata.springeropen.com"
     with webdriver.Firefox() as driver:
-        articles = get_articles(driver, base_url)
-        save_json(
-            "articles-info.json",
-            list(
-                map(
-                    lambda obj: obj.__dict__,
-                    articles
-                )
-            )
-        )
-        pass
+        articles = get_articles_meta(driver, base_url)
+        with open("articles-info.json", 'w', encoding='utf-8') as f:
+            json.dump(articles, f, indent=2)
