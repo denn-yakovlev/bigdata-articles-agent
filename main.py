6a221404effd20ca8ec3b86f104b299604597dc8
Denis Yakovlev <denn.yakovlev@gmail.com>
2020-03-10 18:10:41 +0300

    tags fix

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from selenium import webdriver
 from lxml import html
-from bs4 import BeautifulSoup
 import json
 
 
 def get_page_html_element(driver):
     return html.fromstring(driver.page_source)
+
+
+def get_text(elems_list):
+    return "".join(
+        map(
+            lambda el: el.text if isinstance(el, html.HtmlElement) else el,
+            elems_list
+        )
+    )
 
 
 def get_articles_meta(driver, base_url):
@@ -22,9 +30,9 @@ def get_articles_meta(driver, base_url):
             articles.append(
                 {
                     "url": base_url + link,
-                    "title": str(html_element.xpath("//h1[@data-test='article-title']/text()")[0]),
-                    "category": str(html_element.xpath("//li[@data-test='article-category']/text()")[0]),
-                    "pub_date": str(html_element.xpath("//time[@itemprop='datePublished']/@datetime")[0]),
+                    "title": get_text(html_element.xpath("//h1[@data-test='article-title']/node()")),
+                    "category": get_text(html_element.xpath("//li[@data-test='article-category']/node()")),
+                    "pub_date": get_text(html_element.xpath("//time[@itemprop='datePublished']/@datetime")),
                     "authors": html_element.xpath("//ul[@data-test='authors-list']/li/span[1]/a/text()"),
                     "keywords": html_element.xpath("//ul[@class='c-article-subject-list']/li/span/text()")
                 }
