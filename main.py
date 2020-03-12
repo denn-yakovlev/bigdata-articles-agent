97fdb18a51f8fbe2898311ca811892afd7e10ca8
Denis Yakovlev <denn.yakovlev@gmail.com>
2020-03-12 23:04:37 +0300

    Got articles' texts

--- main.py
+++ main.py
@@ -3,41 +3,60 @@ from lxml import html
 import json
 
 
-def get_page_html_element(driver):
+def get_page_html_element(url, driver):
+    driver.get(url)
     return html.fromstring(driver.page_source)
 
 
-def get_text(elems_list):
-    return "".join(
-        map(
-            lambda el: el.text if isinstance(el, html.HtmlElement) else el,
-            elems_list
-        )
-    )
+def get_whole_text(texts_list):
+    return "".join(texts_list)
 
 
 def get_articles_meta(driver, base_url):
     articles = []
-    for page in range(1, 7):
-        driver.get(f"{base_url}/articles?searchType=journalSearch&sort=PubDate&page={page}")
-        html_element = get_page_html_element(driver)
+    for page in range(1, 2):
+        html_element = get_page_html_element(
+            f"{base_url}/articles?searchType=journalSearch&sort=PubDate&page={page}",
+            driver
+        )
         articles_links = html_element.xpath(
             "//a[@itemprop='url' and @data-test='title-link']/@href"
         )
         for link in articles_links:
-            driver.get(base_url + link)
-            html_element = get_page_html_element(driver)
+            html_element = get_page_html_element(base_url + link, driver)
             articles.append(
                 {
                     "url": base_url + link,
-                    "title": get_text(html_element.xpath("//h1[@data-test='article-title']/node()")),
-                    "category": get_text(html_element.xpath("//li[@data-test='article-category']/node()")),
-                    "pub_date": get_text(html_element.xpath("//time[@itemprop='datePublished']/@datetime")),
+                    "title": get_whole_text(
+                        html_element.xpath(
+                            "//h1[@data-test='article-title']/descendant::text()"
+                        )
+                    ),
+                    "category": get_whole_text(
+                        html_element.xpath(
+                            "//li[@data-test='article-category']/descendant::text()"
+                        )
+                    ),
+                    "pub_date": get_whole_text(
+                        html_element.xpath(
+                            "//time[@itemprop='datePublished']/@datetime"
+                        )
+                    ),
                     "authors": html_element.xpath("//ul[@data-test='authors-list']/li/span[1]/a/text()"),
-                    "keywords": html_element.xpath("//ul[@class='c-article-subject-list']/li/span/text()")
+                    "keywords": html_element.xpath("//ul[@class='c-article-subject-list']/li/span/text()"),
+                    "text": parse_article(html_element)
                 }
             )
     return articles
+
+
+def parse_article(html_element):
+    result = {}
+    section: html.HtmlElement
+    for section in html_element.xpath("//article/section"):
+        sect_header = get_whole_text(section.xpath("./div/h2/descendant::text()"))
+        result[sect_header] = "".join(section.xpath("./div/div/descendant::text()"))
+    return result
 
 
 if __name__ == '__main__':
