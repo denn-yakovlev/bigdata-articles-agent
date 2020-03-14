aa23fe930b392ac25a42ce26f2d3f12ac89af986
Denis Yakovlev <denn.yakovlev@gmail.com>
2020-03-14 14:47:52 +0300

    Figures parsed

--- main.py
+++ main.py
@@ -8,10 +8,6 @@ def get_page_html_element(url, driver):
     return html.fromstring(driver.page_source)
 
 
-def get_whole_text(texts_list):
-    return "".join(texts_list)
-
-
 def get_articles_meta(driver, base_url):
     articles = []
     for page in range(1, 2):
@@ -27,37 +23,48 @@ def get_articles_meta(driver, base_url):
             articles.append(
                 {
                     "url": base_url + link,
-                    "title": get_whole_text(
+                    "title": "".join(
                         html_element.xpath(
                             "//h1[@data-test='article-title']/descendant::text()"
                         )
                     ),
-                    "category": get_whole_text(
+                    "category": "".join(
                         html_element.xpath(
                             "//li[@data-test='article-category']/descendant::text()"
                         )
                     ),
-                    "pub_date": get_whole_text(
+                    "pub_date": "".join(
                         html_element.xpath(
                             "//time[@itemprop='datePublished']/@datetime"
                         )
                     ),
                     "authors": html_element.xpath("//ul[@data-test='authors-list']/li/span[1]/a/text()"),
                     "keywords": html_element.xpath("//ul[@class='c-article-subject-list']/li/span/text()"),
-                    "text": parse_article(html_element)
+                    "text": parse_article_for_text(html_element),
+                    "figures": parse_article_for_figures(html_element)
                 }
             )
     return articles
 
 
-def parse_article(html_element):
+def parse_article_for_text(html_element):
     result = {}
     section: html.HtmlElement
     for section in html_element.xpath("//article/section"):
-        sect_header = get_whole_text(section.xpath("./div/h2/descendant::text()"))
-        result[sect_header] = "".join(section.xpath("./div/div/descendant::text()"))
+        sect_header = "".join(section.xpath("div/h2/descendant::text()"))
+        result[sect_header] = "".join(section.xpath("div/div/descendant::text()"))
     return result
 
+
+def parse_article_for_figures(html_element):
+    result = []
+    for figure in html_element.xpath("//figure"):
+       result.append({
+            "caption": "".join(figure.xpath("figcaption/descendant::text()")),
+            "img_link": figure.xpath("descendant::a/@href"),
+            "picture_src": figure.xpath("descendant::picture/img/src")
+       })
+    return result
 
 if __name__ == '__main__':
     base_url = "https://journalofbigdata.springeropen.com"
