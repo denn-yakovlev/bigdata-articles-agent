57b2f4f8df1b083663598a8d6cea91be17a739a7
Denis Yakovlev <denn.yakovlev@gmail.com>
2020-03-29 20:04:59 +0300

    Authors remade

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.common import exceptions
 from lxml import html
 import json
 
@@ -10,7 +11,7 @@ def get_page_html_element(url, driver):
 
 def get_articles_meta(driver, base_url):
     articles = []
-    for page in range(1, 2):
+    for page in range(1, 7):
         html_element = get_page_html_element(
             f"{base_url}/articles?searchType=journalSearch&sort=PubDate&page={page}",
             driver
@@ -38,16 +39,32 @@ def get_articles_meta(driver, base_url):
                             "//time[@itemprop='datePublished']/@datetime"
                         )
                     ),
-                    "authors": html_element.xpath("//ul[@data-test='authors-list']/li/span[1]/a/text()"),
+                    "authors": get_authors_info(html_element),
                     "keywords": html_element.xpath("//ul[@class='c-article-subject-list']/li/span/text()"),
-                    "text": parse_article_for_text(html_element),
-                    "figures": parse_article_for_figures(html_element)
+                    "text": get_text(html_element),
+                    "figures": get_figures_info(html_element)
                 }
             )
     return articles
 
 
-def parse_article_for_text(html_element):
+def get_authors_info(html_element):
+    result = []
+    for author in html_element.xpath("//ul[@data-test='authors-list']/li[@itemprop='author']"):
+        name = author.xpath("span[1]/a/text()")[0]
+        driver.find_element_by_link_text(name).click()
+        try:
+            link = driver.find_element_by_class_name("c-article-orcid").get_attribute("href")
+        except exceptions.NoSuchElementException:
+            link = None
+        result.append({
+            "name": name,
+            "link": link
+        })
+    return result
+
+
+def get_text(html_element):
     result = {}
     section: html.HtmlElement
     for section in html_element.xpath("//article/section"):
@@ -56,15 +73,20 @@ def parse_article_for_text(html_element):
     return result
 
 
-def parse_article_for_figures(html_element):
+def get_figures_info(html_element):
     result = []
-    for figure in html_element.xpath("//figure"):
-       result.append({
-            "caption": "".join(figure.xpath("figcaption/descendant::text()")),
-            "img_link": figure.xpath("descendant::a/@href"),
-            "picture_src": figure.xpath("descendant::picture/img/src")
-       })
+    figure: html.HtmlElement
+    for figure in html_element.xpath("//article//figure"):
+        caption = "".join(figure.xpath(".//figcaption//descendant::text()"))
+        link = figure.find(".//a")
+        src = figure.find(".//picture//img")
+        result.append({
+            "caption": caption,
+            "img_link": link.get("href") if link != None else None,
+            "picture_src": src.get("src") if src != None else None
+        })
     return result
+
 
 if __name__ == '__main__':
     base_url = "https://journalofbigdata.springeropen.com"
